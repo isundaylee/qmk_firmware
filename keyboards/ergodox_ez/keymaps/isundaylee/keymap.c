970280f0fb8dca93a7b3ae71e2819f9c2936d0f7
Jiahao Li <reg@ljh.me>
2019-06-21 21:23:12 -0400

    Removes left-over NUMS keys

--- keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
+++ keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _,                _,                _,                _,                _,                _,                _,                _,                _,                KC_KP_7,          KC_KP_8,          KC_KP_9,          KC_KP_MINUS,      _,    
   _,                _,                _,                _,                _,                _,                                                    _,                KC_KP_4,          KC_KP_5,          KC_KP_6,          KC_KP_PLUS,       _,    
   _,                _,                _,                _,                _,                _,                _,                _,                _,                KC_KP_1,          KC_KP_2,          KC_KP_3,          KC_KP_ENTER,      _,    
-  _,                KC_KP_ENTER,      KC_KP_DOT,        KC_KP_0,          KC_KP_0,                                                                                  KC_KP_0,          KC_KP_0,          KC_KP_DOT,        KC_KP_ENTER,      _,    
+  _,                _,                _,                _,                _,                                                                                        KC_KP_0,          KC_KP_0,          KC_KP_DOT,        KC_KP_ENTER,      _,    
 
                                                                           _,                _,                                                    _,                _,    
                                                                                             _,                                                    _,    
