60fbead5ebe0f03222813983de1f5b75d9cb2ca2
Jiahao Li <reg@ljh.me>
2019-06-21 21:06:52 -0400

    Makes KC_NO look nicer

--- keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
+++ keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
@@ -20,11 +20,11 @@
     { L02, L12, L22, L32, L42, L52 },     \
     { L03, L13, L23, L33, L43, L53 },     \
     { L04, L14, L24, L34, L44, L54 },     \
-    { L05, L15, L25, L35, KC_NO, L55 },   \
-    { L06, L16, KC_NO, L36, KC_NO, L56 }, \
+    { L05, L15, L25, L35, _,     L55 },   \
+    { L06, L16, _,     L36, _,     L56 }, \
                                           \
-    { R00, R10, KC_NO, R30,KC_NO, R50 },  \
-    { R01, R11, R21, R31,KC_NO, R51 },    \
+    { R00, R10, _,     R30,_,     R50 },  \
+    { R01, R11, R21, R31,_,     R51 },    \
     { R02, R12, R22, R32, R42, R52 },     \
     { R03, R13, R23, R33, R43, R53 },     \
     { R04, R14, R24, R34, R44, R54 },     \
@@ -43,68 +43,72 @@ enum custom_keycodes {
   RGB_SLD
 };
 
+#define _ KC_NO
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_ergodox_flip_pretty(
-  KC_GRAVE,         KC_1,             KC_2,             KC_3,             KC_4,             KC_5,             KC_6,             KC_7,             KC_8,             KC_9,             KC_0,             KC_MINS,          KC_EQL,           KC_NO,
-  KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             KC_NO,            KC_NO,            KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             KC_NO,
+  KC_GRAVE,         KC_1,             KC_2,             KC_3,             KC_4,             KC_5,             KC_6,             KC_7,             KC_8,             KC_9,             KC_0,             KC_MINS,          KC_EQL,           _,    
+  KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             _,                _,                KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             _,    
   LCTL_T(KC_ESC),   KC_A,             KC_S,             KC_D,             KC_F,             KC_G,                                                 KC_H,             KC_J,             KC_K,             KC_L,             KC_SCOLON,        KC_RCTRL,
-  KC_LSPO,          KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             KC_NO,            KC_NO,            KC_N,             KC_M,             KC_COMMA,         KC_DOT,           KC_SLASH,         KC_RSPC,
-  MO(SYMB),         MO(NUMS),         KC_NO,            KC_LALT,          KC_LCMD,                                                                                  KC_LEFT,          KC_DOWN,          KC_UP,            KC_RIGHT,         KC_NO,
+  KC_LSPO,          KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             _,                _,                KC_N,             KC_M,             KC_COMMA,         KC_DOT,           KC_SLASH,         KC_RSPC,
+  MO(SYMB),         MO(NUMS),         _,                KC_LALT,          KC_LCMD,                                                                                  KC_LEFT,          KC_DOWN,          KC_UP,            KC_RIGHT,         _,    
 
-                                                                          KC_NO,            KC_NO,                                                KC_NO,            KC_NO,
-                                                                                            LCAG(KC_NO),                                          KC_NO,
-                                                        KC_SPACE,         KC_LCMD,          MO(SYMB),                                             KC_NO,            KC_BSPACE,        KC_ENTER
+                                                                          _,                _,                                                    _,                _,    
+                                                                                            LCAG(KC_NO),                                          _,    
+                                                        KC_SPACE,         KC_LCMD,          MO(SYMB),                                             _,                KC_BSPACE,        KC_ENTER
 ),
 
 [SYMB] = LAYOUT_ergodox_flip_pretty(
-  KC_NO,            KC_F1,            KC_F2,            KC_F3,            KC_F4,            KC_F5,            KC_F6,            KC_F7,            KC_F8,            KC_F9,            KC_F10,           KC_F11,           KC_F12,           KC_NO,
-  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_MINS,          KC_PLUS,          KC_DQUO,          LSFT(KC_P),       KC_NO, 
-  KC_TRNS,          KC_NO,            KC_NO,            KC_LCBR,          KC_RCBR,          KC_NO,                                                KC_EQL,           KC_LBRC,          KC_RBRC,          KC_QUOTE,         KC_P,             KC_NO,
-  KC_TRNS,          KC_NO,            KC_NO,            SGUI(KC_LBRC),    SGUI(KC_RBRC),    KC_NO,            KC_NO,            KC_NO,            KC_UNDS,          KC_PIPE,          KC_LT,            KC_GT,            KC_BSLASH,        KC_NO,
-  KC_TRNS,          KC_NO,            KC_NO,            KC_TRNS,          KC_TRNS,                                                                                  KC_HOME,          KC_PGDN,          KC_PGUP,          KC_END,           KC_NO,
+  _,                KC_F1,            KC_F2,            KC_F3,            KC_F4,            KC_F5,            KC_F6,            KC_F7,            KC_F8,            KC_F9,            KC_F10,           KC_F11,           KC_F12,           _,    
+  _,                _,                _,                _,                _,                _,                _,                _,                _,                KC_MINS,          KC_PLUS,          KC_DQUO,          LSFT(KC_P),       _,     
+  KC_TRNS,          _,                _,                KC_LCBR,          KC_RCBR,          _,                                                    KC_EQL,           KC_LBRC,          KC_RBRC,          KC_QUOTE,         KC_P,             _,    
+  KC_TRNS,          _,                _,                SGUI(KC_LBRC),    SGUI(KC_RBRC),    _,                _,                _,                KC_UNDS,          KC_PIPE,          KC_LT,            KC_GT,            KC_BSLASH,        _,    
+  KC_TRNS,          _,                _,                KC_TRNS,          KC_TRNS,                                                                                  KC_HOME,          KC_PGDN,          KC_PGUP,          KC_END,           _,    
 
-                                                                          KC_NO,            KC_NO,                                                DF(COOK),         KC_NO,
-                                                                                            KC_NO,                                                KC_NO,
-                                                        KC_NO,            KC_TRNS,          KC_NO,                                                KC_NO,            KC_DELETE,        KC_NO
+                                                                          _,                _,                                                    DF(COOK),         _,    
+                                                                                            _,                                                    _,    
+                                                        _,                KC_TRNS,          _,                                                    _,                KC_DELETE,        KC_NO
 ),
 
 [NUMS] = LAYOUT_ergodox_flip_pretty(
-  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_KP_SLASH,      KC_KP_ASTERISK,   KC_NO,
-  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_KP_7,          KC_KP_8,          KC_KP_9,          KC_KP_MINUS,      KC_NO,
-  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,                                                KC_NO,            KC_KP_4,          KC_KP_5,          KC_KP_6,          KC_KP_PLUS,       KC_NO,
-  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_KP_1,          KC_KP_2,          KC_KP_3,          KC_KP_ENTER,      KC_NO,
-  KC_NO,            KC_KP_ENTER,      KC_KP_DOT,        KC_KP_0,          KC_KP_0,                                                                                  KC_KP_0,          KC_KP_0,          KC_KP_DOT,        KC_KP_ENTER,      KC_NO,
+  _,                _,                _,                _,                _,                _,                _,                _,                _,                _,                _,                KC_KP_SLASH,      KC_KP_ASTERISK,   _,    
+  _,                _,                _,                _,                _,                _,                _,                _,                _,                KC_KP_7,          KC_KP_8,          KC_KP_9,          KC_KP_MINUS,      _,    
+  _,                _,                _,                _,                _,                _,                                                    _,                KC_KP_4,          KC_KP_5,          KC_KP_6,          KC_KP_PLUS,       _,    
+  _,                _,                _,                _,                _,                _,                _,                _,                _,                KC_KP_1,          KC_KP_2,          KC_KP_3,          KC_KP_ENTER,      _,    
+  _,                KC_KP_ENTER,      KC_KP_DOT,        KC_KP_0,          KC_KP_0,                                                                                  KC_KP_0,          KC_KP_0,          KC_KP_DOT,        KC_KP_ENTER,      _,    
 
-                                                                          KC_NO,            KC_NO,                                                KC_NO,            KC_NO,
-                                                                                            KC_NO,                                                KC_NO,
-                                                        KC_NO,            KC_NO,            KC_NO,                                                KC_NO,            KC_NO,            KC_ENTER
+                                                                          _,                _,                                                    _,                _,    
+                                                                                            _,                                                    _,    
+                                                        _,                _,                _,                                                    _,                _,                KC_ENTER
 ),
 
 [COOK] = LAYOUT_ergodox(
   // RIGHT
-  KC_ESC,            KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_NO,
-  KC_NO,            KC_NO,        KC_UP,          KC_NO,    KC_NO,    KC_NO,    KC_RSFT,
-  KC_RSFT,          KC_RIGHT,     KC_DOWN,        KC_LEFT,  KC_NO,    KC_NO,
-  KC_RCTRL,         KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_RCTRL,
-  KC_NO,            KC_NO,        KC_NO,          KC_NO,    KC_NO,
+  KC_ESC,            _,            _,              _,        _,        _,        _,    
+  _,                _,            KC_UP,          _,        _,        _,        KC_RSFT,
+  KC_RSFT,          KC_RIGHT,     KC_DOWN,        KC_LEFT,  _,        _,    
+  KC_RCTRL,         _,            _,              _,        _,        _,        KC_RCTRL,
+  _,                _,            _,              _,        _,    
 
-  KC_NO,            DF(BASE),
-  KC_NO,
-  KC_NO,            KC_NO,        KC_NO,
+  _,                DF(BASE),
+  _,    
+  _,                _,            _,    
 
   // LEFT
-  KC_NO,            KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_ESC,
-  KC_LSFT,          KC_NO,        KC_NO,          KC_NO,    KC_W,     KC_NO,    KC_NO,
-                    KC_NO,        KC_NO,          KC_D,     KC_S,     KC_A,     KC_LSFT,
-  KC_LCTRL,         KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_LCTRL,
-                                  KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_NO,
+  _,                _,            _,              _,        _,        _,        KC_ESC,
+  KC_LSFT,          _,            _,              _,        KC_W,     _,        _,    
+                    _,            _,              KC_D,     KC_S,     KC_A,     KC_LSFT,
+  KC_LCTRL,         _,            _,              _,        _,        _,        KC_LCTRL,
+                                  _,              _,        _,        _,        _,    
 
-  KC_NO,            KC_NO,
-  KC_NO,
-  KC_NO,            KC_NO,        KC_NO
+  _,                _,    
+  _,    
+  _,                _,            KC_NO
   ),
 };
+
+#undef _
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
