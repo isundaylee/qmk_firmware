4e557451cb82ca1c64414a4f8625534d20938357
Jiahao Li <reg@ljh.me>
2019-06-21 20:59:33 -0400

    Finally gets rid of flipped ergodox ez keymap :)

--- keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
+++ keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
@@ -2,6 +2,36 @@
 
 #include "version.h"
 
+/*  ---------- LEFT HAND -----------   ---------- RIGHT HAND ---------- */
+#define LAYOUT_ergodox_flip_pretty(                                      \
+    R06,R05,R04,R03,R02,R01,R00,           L06,L05,L04,L03,L02,L01,L00,  \
+    R16,R15,R14,R13,R12,R11,R10,           L16,L15,L14,L13,L12,L11,L10,  \
+    R26,R25,R24,R23,R22,R21,                   L25,L24,L23,L22,L21,L20,  \
+    R36,R35,R34,R33,R32,R31,R30,           L36,L35,L34,L33,L32,L31,L30,  \
+    R46,R45,R44,R43,R42,                           L44,L43,L42,L41,L40,  \
+                            R51,R50,   L56,L55,                          \
+                                R52,   L54,                              \
+                        R53,R54,R55,   L51,L52,L53 )                     \
+                                          \
+   /* matrix positions */                 \
+    {                                     \
+    { L00, L10, L20, L30, L40, KC_NO },   \
+    { L01, L11, L21, L31, L41, L51 },     \
+    { L02, L12, L22, L32, L42, L52 },     \
+    { L03, L13, L23, L33, L43, L53 },     \
+    { L04, L14, L24, L34, L44, L54 },     \
+    { L05, L15, L25, L35, KC_NO, L55 },   \
+    { L06, L16, KC_NO, L36, KC_NO, L56 }, \
+                                          \
+    { R00, R10, KC_NO, R30,KC_NO, R50 },  \
+    { R01, R11, R21, R31,KC_NO, R51 },    \
+    { R02, R12, R22, R32, R42, R52 },     \
+    { R03, R13, R23, R33, R43, R53 },     \
+    { R04, R14, R24, R34, R44, R54 },     \
+    { R05, R15, R25, R35, R45, R55 },     \
+    { R06, R16, R26, R36, R46, KC_NO }    \
+    }
+
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define NUMS 2
@@ -14,76 +44,41 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[BASE] = LAYOUT_ergodox(
-  // RIGHT
-  KC_NO,            KC_EQL,       KC_MINS,        KC_0,     KC_9,     KC_8,     KC_7,
-  KC_NO,            KC_P,         KC_O,           KC_I,     KC_U,     KC_Y,     KC_NO,
-  KC_RCTRL,         KC_SCOLON,    KC_L,           KC_K,     KC_J,     KC_H,
-  KC_RSPC,          KC_SLASH,     KC_DOT,         KC_COMMA, KC_M,     KC_N,     KC_NO,
-  KC_NO,            KC_RIGHT,     KC_UP,          KC_DOWN,  KC_LEFT,
 
-  KC_NO,            KC_NO,
-  KC_NO,
-  KC_ENTER,         KC_BSPACE,    KC_NO,
+[BASE] = LAYOUT_ergodox_flip_pretty(
+  KC_GRAVE,         KC_1,             KC_2,             KC_3,             KC_4,             KC_5,             KC_6,             KC_7,             KC_8,             KC_9,             KC_0,             KC_MINS,          KC_EQL,           KC_NO,
+  KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             KC_NO,            KC_NO,            KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             KC_NO,
+  LCTL_T(KC_ESC),   KC_A,             KC_S,             KC_D,             KC_F,             KC_G,                                                 KC_H,             KC_J,             KC_K,             KC_L,             KC_SCOLON,        KC_RCTRL,
+  KC_LSPO,          KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             KC_NO,            KC_NO,            KC_N,             KC_M,             KC_COMMA,         KC_DOT,           KC_SLASH,         KC_RSPC,
+  MO(SYMB),         MO(NUMS),         KC_NO,            KC_LALT,          KC_LCMD,                                                                                  KC_LEFT,          KC_DOWN,          KC_UP,            KC_RIGHT,         KC_NO,
 
-  // LEFT
-  KC_6,             KC_5,         KC_4,           KC_3,     KC_2,     KC_1,     KC_GRAVE,
-  KC_NO,            KC_T,         KC_R,           KC_E,     KC_W,     KC_Q,     KC_TAB,
-                    KC_G,         KC_F,           KC_D,     KC_S,     KC_A,     LCTL_T(KC_ESC),
-  KC_NO,            KC_B,         KC_V,           KC_C,     KC_X,     KC_Z,     KC_LSPO,
-                                  KC_LCMD,        KC_LALT,  KC_NO,    MO(NUMS), MO(SYMB),
-
-  KC_NO,            KC_NO,
-  LCAG(KC_NO),
-  MO(SYMB),         KC_LCMD,      KC_SPACE
+                                                                          KC_NO,            KC_NO,                                                KC_NO,            KC_NO,
+                                                                                            LCAG(KC_NO),                                          KC_NO,
+                                                        KC_SPACE,         KC_LCMD,          MO(SYMB),                                             KC_NO,            KC_BSPACE,        KC_ENTER
 ),
 
-[SYMB] = LAYOUT_ergodox(
-  // RIGHT
-  KC_NO,            KC_F12,       KC_F11,         KC_F10,   KC_F9,    KC_F8,    KC_F7,
-  KC_NO,            LSFT(KC_P),   KC_DQUO,        KC_PLUS,  KC_MINS,  KC_NO,    KC_NO,
-  KC_NO,            KC_P,         KC_QUOTE,       KC_RBRC,  KC_LBRC,  KC_EQL,
-  KC_NO,            KC_BSLASH,    KC_GT,          KC_LT,    KC_PIPE,  KC_UNDS,  KC_NO,
-  KC_NO,            KC_END,       KC_PGUP,        KC_PGDN,  KC_HOME,
+[SYMB] = LAYOUT_ergodox_flip_pretty(
+  KC_NO,            KC_F1,            KC_F2,            KC_F3,            KC_F4,            KC_F5,            KC_F6,            KC_F7,            KC_F8,            KC_F9,            KC_F10,           KC_F11,           KC_F12,           KC_NO,
+  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_MINS,          KC_PLUS,          KC_DQUO,          LSFT(KC_P),       KC_NO, 
+  KC_TRNS,          KC_NO,            KC_NO,            KC_LCBR,          KC_RCBR,          KC_NO,                                                KC_EQL,           KC_LBRC,          KC_RBRC,          KC_QUOTE,         KC_P,             KC_NO,
+  KC_TRNS,          KC_NO,            KC_NO,            SGUI(KC_LBRC),    SGUI(KC_RBRC),    KC_NO,            KC_NO,            KC_NO,            KC_UNDS,          KC_PIPE,          KC_LT,            KC_GT,            KC_BSLASH,        KC_NO,
+  KC_TRNS,          KC_NO,            KC_NO,            KC_TRNS,          KC_TRNS,                                                                                  KC_HOME,          KC_PGDN,          KC_PGUP,          KC_END,           KC_NO,
 
-  KC_NO,            DF(COOK),
-  KC_NO,
-  KC_NO,            KC_DELETE,    KC_NO,
-
-  // LEFT
-  KC_F6,            KC_F5,        KC_F4,          KC_F3,            KC_F2,    KC_F1,    KC_NO,
-  KC_NO,            KC_NO,        KC_NO,          KC_NO,            KC_NO,    KC_NO,    KC_NO,
-                    KC_NO,        KC_RCBR,        KC_LCBR,          KC_NO,    KC_NO,    KC_TRNS,
-  KC_NO,            KC_NO,        SGUI(KC_RBRC),  SGUI(KC_LBRC),    KC_NO,    KC_NO,    KC_TRNS,
-                                  KC_TRNS,        KC_TRNS,          KC_NO,    KC_NO,    KC_TRNS,
-
-  KC_NO,            KC_NO,
-  KC_NO,
-  KC_NO,            KC_TRNS,      KC_NO
+                                                                          KC_NO,            KC_NO,                                                DF(COOK),         KC_NO,
+                                                                                            KC_NO,                                                KC_NO,
+                                                        KC_NO,            KC_TRNS,          KC_NO,                                                KC_NO,            KC_DELETE,        KC_NO
 ),
 
-[NUMS] = LAYOUT_ergodox(
-  // RIGHT
-  KC_NO,            KC_NO,            KC_KP_ASTERISK,     KC_KP_SLASH,      KC_NO,        KC_NO,    KC_NO,
-  KC_NO,            KC_KP_MINUS,      KC_KP_9,            KC_KP_8,          KC_KP_7,      KC_NO,    KC_NO,
-  KC_NO,            KC_KP_PLUS,       KC_KP_6,            KC_KP_5,          KC_KP_4,      KC_NO,
-  KC_NO,            KC_KP_ENTER,      KC_KP_3,            KC_KP_2,          KC_KP_1,      KC_NO,    KC_NO,
-  KC_NO,            KC_KP_ENTER,      KC_KP_DOT,          KC_KP_0,          KC_KP_0,
+[NUMS] = LAYOUT_ergodox_flip_pretty(
+  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_KP_SLASH,      KC_KP_ASTERISK,   KC_NO,
+  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_KP_7,          KC_KP_8,          KC_KP_9,          KC_KP_MINUS,      KC_NO,
+  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,                                                KC_NO,            KC_KP_4,          KC_KP_5,          KC_KP_6,          KC_KP_PLUS,       KC_NO,
+  KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_KP_1,          KC_KP_2,          KC_KP_3,          KC_KP_ENTER,      KC_NO,
+  KC_NO,            KC_KP_ENTER,      KC_KP_DOT,        KC_KP_0,          KC_KP_0,                                                                                  KC_KP_0,          KC_KP_0,          KC_KP_DOT,        KC_KP_ENTER,      KC_NO,
 
-  KC_NO,            KC_NO,
-  KC_NO,
-  KC_ENTER,         KC_NO,        KC_NO,
-
-  // LEFT
-  KC_NO,            KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_NO,
-  KC_NO,            KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                    KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_NO,
-  KC_NO,            KC_NO,        KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                                  KC_NO,          KC_NO,    KC_NO,    KC_NO,    KC_NO,
-
-  KC_NO,            KC_NO,
-  KC_NO,
-  KC_NO,            KC_NO,        KC_NO
+                                                                          KC_NO,            KC_NO,                                                KC_NO,            KC_NO,
+                                                                                            KC_NO,                                                KC_NO,
+                                                        KC_NO,            KC_NO,            KC_NO,                                                KC_NO,            KC_NO,            KC_ENTER
 ),
 
 [COOK] = LAYOUT_ergodox(
