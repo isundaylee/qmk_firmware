3166fafa70090c875353f32994da3faa6f6d387b
Jiahao Li <reg@ljh.me>
2019-06-24 14:27:48 -0400

    Makes SYMB layer momentary only

--- keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
+++ keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             _,                /* | */             _,                KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             _,    
   LCTL_T(KC_ESC),   KC_A,             KC_S,             KC_D,             KC_F,             KC_G,                               /* | */                               KC_H,             KC_J,             KC_K,             KC_L,             KC_SCOLON,        KC_RCTRL,
   KC_LSPO,          KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             _,                /* | */             _,                KC_N,             KC_M,             KC_COMMA,         KC_DOT,           KC_SLASH,         KC_RSPC,
-  TT(SYMB),         TT(NUMS),         _,                KC_LALT,          KC_LCMD,                                              /* | */                                                 KC_LEFT,          KC_DOWN,          KC_UP,            KC_RIGHT,         _,    
+  MO(SYMB),         TT(NUMS),         _,                KC_LALT,          KC_LCMD,                                              /* | */                                                 KC_LEFT,          KC_DOWN,          KC_UP,            KC_RIGHT,         _,    
                                                                                                                                 /* | */
                                                                           _,                _,                                  /* | */                               _,                _,    
                                                                                             LCAG(KC_NO),                        /* | */                               _,    
-                                                        KC_SPACE,         KC_LCMD,          TT(SYMB),                           /* | */                               TT(SYMB),         KC_BSPACE,        KC_ENTER
+                                                        KC_SPACE,         KC_LCMD,          MO(SYMB),                           /* | */                               MO(SYMB),         KC_BSPACE,        KC_ENTER
 ),
 
 [SYMB] = LAYOUT_ergodox_flip_pretty(
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                                                 /* | */
                                                                           _,                _,                                  /* | */                               _,                _,    
                                                                                             _,                                  /* | */                               _,    
-                                                        _,                KC_TRNS,          TT(SYMB),                           /* | */                               KC_TRNS,          KC_DELETE,        KC_NO
+                                                        _,                KC_TRNS,          _,                                  /* | */                               KC_TRNS,          KC_DELETE,        KC_NO
 ),
 
 [NUMS] = LAYOUT_ergodox_flip_pretty(
