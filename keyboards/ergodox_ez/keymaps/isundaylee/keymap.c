472ab641037682cca563b8315aa3bea0a3ee2fa4
Jiahao Li <reg@ljh.me>
2019-06-24 23:05:57 -0400

    Adds TAB key in NUMS layer

--- keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
+++ keyboards/ergodox_ez/keymaps/isundaylee/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [NUMS] = LAYOUT_ergodox_flip_pretty(
   _,                _,                _,                _,                _,                _,                _,                /* | */             _,                _,                _,                KC_KP_SLASH,      KC_KP_ASTERISK,   KC_KP_MINUS,      _,
-  _,                _,                _,                _,                _,                _,                _,                /* | */             _,                _,                KC_KP_7,          KC_KP_8,          KC_KP_9,          KC_KP_MINUS,      _,    
+  KC_TAB,           _,                _,                _,                _,                _,                _,                /* | */             _,                _,                KC_KP_7,          KC_KP_8,          KC_KP_9,          KC_KP_MINUS,      _,    
   _,                _,                _,                _,                _,                _,                                  /* | */                               _,                KC_KP_4,          KC_KP_5,          KC_KP_6,          KC_KP_PLUS,       _,    
   _,                _,                _,                _,                _,                _,                _,                /* | */             _,                _,                KC_KP_1,          KC_KP_2,          KC_KP_3,          KC_KP_ENTER,      _,    
   _,                KC_TRNS,          _,                _,                _,                                                    /* | */                                                 KC_KP_0,          KC_KP_0,          KC_KP_DOT,        KC_KP_ENTER,      _,    
